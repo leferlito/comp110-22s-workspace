c01b918917bdf6a431fb400e66ad3c983645e9e4
Lauren Ferlito <lferlito@unc.edu>
2022-01-18 12:29:49 -0500

    LS06

---
+++ lessons/input.py
@@ -0,0 +1,5 @@
+"""Demonstrates asking the user for input."""
+
+my_first_name: str = input("What is your name? ")
+print("Hello, " + my_first_name + ", good morning!")
+print("You are awesome, " + my_first_name)
