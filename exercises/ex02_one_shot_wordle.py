58887f6340fc3b99d19b7745a23a25c5b5e2fb36
Lauren Ferlito <lferlito@unc.edu>
2022-01-29 21:47:43 -0500

    EX02 Progress 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,24 +3,24 @@
 __author__ = "730483024"
 
 secret: str = "python"
-guess: str = input(f"What is your {len(secret)}-letter guess? ")
+the_guess: str = input(f"What is your {len(secret)}-letter guess? ")
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 i: int = 0
 answer: str = ""
 
-while len(guess) != len(secret):
-    guess: str = input(f"That was not {len(secret)} letters! Try again: ")
+while len(the_guess) != len(secret):
+    the_guess: str = input(f"That was not {len(secret)} letters! Try again: ")
 
 while i < len(secret):
     alternate_indices: int = 0
     exist_elsewhere: bool = False
-    if guess[i] == secret[i]:
+    if the_guess[i] == secret[i]:
         answer = answer + GREEN_BOX
-    if guess[i] != secret[i] and i < len(secret):
+    if the_guess[i] != secret[i] and i < len(secret):
         while alternate_indices < len(secret) and not exist_elsewhere:
-            if guess[i] == secret[alternate_indices]:
+            if the_guess[i] == secret[alternate_indices]:
                 exist_elsewhere = True
             else:
                 alternate_indices += 1
@@ -30,7 +30,7 @@ while i < len(secret):
             answer = answer + WHITE_BOX
     i = i + 1
 
-if guess != secret:
+if the_guess != secret:
     print(f"{answer} \nNot quite. Play again soon! ")
-if guess == secret:
+if the_guess == secret:
     print(f"{answer} \nWoo! You got it! ")
