2cd95f2a88cf2aa4f9aa68529820f1ab91e0dd1d
Lauren Ferlito <lferlito@unc.edu>
2022-01-29 21:06:59 -0500

    ex02 more progress

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -8,14 +8,16 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 i: int = 0
-ANSWER: str = ""
+answer: str = ""
+
 while len(guess) != len(secret):
-    guess: str = input("That was not 6 letters! Try again: ")
+    guess: str = input(f"That was not {len(secret)} letters! Try again: ")
+
 while i < len(secret):
     alternate_indices: int = 0
     exist_elsewhere: bool = False
     if guess[i] == secret[i]:
-        ANSWER = ANSWER + GREEN_BOX
+        answer = answer + GREEN_BOX
     if guess[i] != secret[i] and i < len(secret):
         while alternate_indices < len(secret) and not exist_elsewhere:
             if guess[i] == secret[alternate_indices]:
@@ -23,13 +25,12 @@ while i < len(secret):
             else:
                 alternate_indices += 1
         if exist_elsewhere:
-            ANSWER = ANSWER + YELLOW_BOX
+            answer = answer + YELLOW_BOX
         if not exist_elsewhere:
-            ANSWER = ANSWER + WHITE_BOX
+            answer = answer + WHITE_BOX
     i = i + 1
-print(ANSWER)
+
 if guess != secret:
-    print("Not quite. Play again soon! ")
+    print(f"{answer} \nNot quite. Play again soon! ")
 if guess == secret:
-    print("Woo! You got it! ")
-exit()
+    print(f"{answer} \nWoo! You got it! ")
