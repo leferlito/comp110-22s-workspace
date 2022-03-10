601446a814fec6542ba2f0aef2bb42af8f01edc5
Lauren Ferlito <lferlito@unc.edu>
2022-03-10 11:09:14 -0500

    ex07

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -1,5 +1,16 @@
 """Dictionary related utility functions."""
 
-__author__ = ""
+__author__ = "730483024"
 
-# Define your functions below
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]: 
+    """Read the rows of a CSV into a 'table"."""
+    result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader: 
+        result.append(row)
+    file_handle.close()
+    return result
