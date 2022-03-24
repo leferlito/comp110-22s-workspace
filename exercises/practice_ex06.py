3bbeee5f2c2946ea024c1de8610b6d7430f237bf
Lauren Ferlito <lferlito@unc.edu>
2022-03-24 19:34:53 -0400

    delete

--- exercises/practice_ex06.py
+++
@@ -1,9 +0,0 @@
-a: dict[str, str] = {}
-a = {'a': 'z', 'b': 'x', 'c': 'x'}
-result: dict[str, str] = {}
-result: dict[str, str] = {}
-for x in a:
-    result[a[x]] = x
-    if result[x] == a[x]: 
-        raise KeyError  # could be cleaner. Why is the message longer than the example?
-print(result)
