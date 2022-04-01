51acb221105740d1fa3a788f8fc849916728825e
Lauren Ferlito <lferlito@unc.edu>
2022-04-01 17:52:09 -0400

    simpy progress

--- exercises/ex09/Simpy.py
+++ exercises/ex09/Simpy.py
@@ -8,6 +8,53 @@ __author__ = "YOUR PID HERE"
 
 
 class Simpy:
-    values: list[float]
+    """Utility class for working with a column of numerical values. Simplified version of NumPy."""
+    
+    values: list[float] = []
 
-    # TODO: Your constructor and methods will go here.
+    def __init__(self, values: list[float]): 
+        """Constructor definition for initializetion of attributes."""
+        self.values = values
+
+    def __str__(self) -> str: 
+        """Convert Simpy object into a str representation."""
+        return f"{self.values}"
+        # Not printing ones
+
+    def __fill__(self, float_value: float, number_of_values: int) -> None: 
+        """..."""
+        i: float = 0.0
+        while i <= number_of_values: 
+            self.values.append(float_value)
+            i += 1.0
+
+    def __arrange__(self, start: float, stop: float, step: float) -> None: 
+        """..."""
+        assert step != 0.0
+        i: float = start
+        self.values.append(start)
+        while i <= stop: 
+            i += step
+            self.values.append(i)
+
+    # def __sum__(self, rhs: float) -> float: 
+    #     total: float
+    #     total = self + rhs
+    #     return total
+
+    def __add__(self, lhs: Union[float, Simpy]) -> Simpy: 
+        """Vectorized concatination operator."""
+        result: list[str] = []
+
+        if isinstance(lhs, float): 
+            for item in self.values:
+                result.append(lhs + item)
+        
+        else: 
+            assert len(self.values) == len(lhs.values)
+            for i in range(0, len(self.values)): 
+                result.append(self.values[i] + lhs.values[i])
+
+        return Simpy(result) 
+
+    
